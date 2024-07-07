35765cc113c1a93a487db313a23525edce60bbde
Taras Shcherban <147646967+Miki535@users.noreply.github.com>
2024-07-07 08:49:23 +0000

    Update

--- main.go
+++ main.go
@@ -1,5 +1,14 @@
 package main
 
-func main() {
+import "github.com/gin-gonic/gin"
 
+func main() {
+	r := gin.Default()
+
+	r.LoadHTMLGlob("templates/*")
+
+	r.GET("/", func(c *gin.Context) {
+		c.HTML(200, "index.html", nil)
+	})
+	r.Run(":8080")
 }
